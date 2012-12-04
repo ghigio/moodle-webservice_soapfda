388f46b5b430b0de5e9e8106d5704032496eb930
Federico Ghigini <federico.ghigini@gmail.com>
2012-12-04 10:09:39 +0100

    updated version number

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2012111300;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2012120400;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2011120500;        // Requires this Moodle version
 $plugin->release   = '2.2 (Build: 2011120500)';
 $plugin->component = 'webservice_soapfda'; // Full name of the plugin (used for diagnostics)
